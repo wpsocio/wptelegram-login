630f47899f5db7b462ee3713ca3474df6d3bbd0c
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-06-16 12:51:31 +0530

    Update packages and translations

--- src/languages/wptelegram-login-js-translations.php
+++ src/languages/wptelegram-login-js-translations.php
@@ -1,220 +1,174 @@
 <?php
 /* THIS IS A GENERATED FILE. DO NOT EDIT DIRECTLY. */
 $generated_i18n_strings = array(
-	// Reference: src/admin/blocks/telegram-login/block.js:42
-	__( 'WP Telegram Login', 'wptelegram-login' ),
-
-	// Reference: src/admin/blocks/telegram-login/block.js:74
-	__( 'Button Settings', 'wptelegram-login' ),
-
-	// Reference: src/admin/blocks/telegram-login/block.js:76
-	// Reference: src/admin/settings/fields.js:79
-	__( 'Button Style', 'wptelegram-login' ),
-
-	// Reference: src/admin/blocks/telegram-login/block.js:86
-	// Reference: src/admin/settings/fields.js:80
-	__( 'Show User Photo', 'wptelegram-login' ),
-
-	// Reference: src/admin/blocks/telegram-login/block.js:91
-	// Reference: src/admin/settings/fields.js:81
-	__( 'Corner Radius', 'wptelegram-login' ),
-
-	// Reference: src/admin/blocks/telegram-login/block.js:99
-	// Reference: src/admin/settings/fields.js:82
+	// Reference: src/admin/blocks/src/telegram-login/block.js:102
+	// Reference: src/admin/settings/src/fields.js:90
 	__( 'Show if user is', 'wptelegram-login' ),
 
-	// Reference: src/admin/settings/components/BotTestResult.js:13
+	// Reference: src/admin/blocks/src/telegram-login/block.js:64
+	__( 'WP Telegram Login', 'wptelegram-login' ),
+
+	// Reference: src/admin/blocks/src/telegram-login/block.js:73
+	__( 'Button Settings', 'wptelegram-login' ),
+
+	// Reference: src/admin/blocks/src/telegram-login/block.js:75
+	// Reference: src/admin/settings/src/fields.js:87
+	__( 'Button Style', 'wptelegram-login' ),
+
+	// Reference: src/admin/blocks/src/telegram-login/block.js:85
+	// Reference: src/admin/settings/src/fields.js:88
+	__( 'Show User Photo', 'wptelegram-login' ),
+
+	// Reference: src/admin/blocks/src/telegram-login/block.js:94
+	// Reference: src/admin/settings/src/fields.js:89
+	__( 'Corner Radius', 'wptelegram-login' ),
+
+	// Reference: src/admin/settings/src/components/BotTestResult.js:8
 	__( 'Test Result:', 'wptelegram-login' ),
 
-	// Reference: src/admin/settings/components/Header.js:35
-	__( 'With this plugin, you can let the users login to your website with their Telegram and make it simple for them to get connected.', 'wptelegram-login' ),
-
-	// Reference: src/admin/settings/components/Instructions.js:16
-	__( 'INSTRUCTIONS!', 'wptelegram-login' ),
-
-	// Reference: src/admin/settings/components/Instructions.js:19
-	__( 'Create a Bot by sending %1$s command to %2$s.', 'wptelegram-login' ),
-
-	// Reference: src/admin/settings/components/Instructions.js:20
-	__( 'After completing the steps %s will provide you the Bot Token.', 'wptelegram-login' ),
-
-	// Reference: src/admin/settings/components/Instructions.js:21
-	__( 'Copy the token and paste into the Bot Token field below.', 'wptelegram-login' ),
-
-	// Reference: src/admin/settings/components/Instructions.js:21
-	__( 'For ease, use %s', 'wptelegram-login' ),
-
-	// Reference: src/admin/settings/components/Instructions.js:22
-	__( 'Send %1$s command to %2$s, select your bot and then send %3$s', 'wptelegram-login' ),
-
-	// Reference: src/admin/settings/components/Instructions.js:23
-	__( 'Test your bot token below and fill in the bot username if not filled automatically.', 'wptelegram-login' ),
-
-	// Reference: src/admin/settings/components/Instructions.js:24
-	__( 'Hit %s below', 'wptelegram-login' ),
-
-	// Reference: src/admin/settings/components/Instructions.js:24
-	// Reference: src/admin/settings/components/SettingsForm.js:254
-	__( 'Save Changes', 'wptelegram-login' ),
-
-	// Reference: src/admin/settings/components/Instructions.js:25
-	__( 'That\'s it. You are ready to rock :)', 'wptelegram-login' ),
-
-	// Reference: src/admin/settings/components/PluginInfoCard.js:20
-	__( 'Let the users login to your WordPress website with their Telegram and make it simple for them to get connected and let them receive their email notifications on Telegram.', 'wptelegram-login' ),
-
-	// Reference: src/admin/settings/components/PluginInfoCard.js:25
-	__( 'Do you like %s?', 'wptelegram-login' ),
-
-	// Reference: src/admin/settings/components/PluginInfoCard.js:32
-	__( 'Need help?', 'wptelegram-login' ),
-
-	// Reference: src/admin/settings/components/PluginInfoCard.js:33
-	__( 'Get LIVE support on Telegram', 'wptelegram-login' ),
-
-	// Reference: src/admin/settings/components/SettingsForm.js:101
-	__( 'Login Options', 'wptelegram-login' ),
-
-	// Reference: src/admin/settings/components/SettingsForm.js:105
-	__( 'Disable Sign up', 'wptelegram-login' ),
-
-	// Reference: src/admin/settings/components/SettingsForm.js:106
-	__( 'If checked, only the existing users who have connected their Telegram will be able to login.', 'wptelegram-login' ),
-
-	// Reference: src/admin/settings/components/SettingsForm.js:115
+	// Reference: src/admin/settings/src/components/FormRenderer.js:111
 	__( 'The default role to assign for the new users', 'wptelegram-login' ),
 
-	// Reference: src/admin/settings/components/SettingsForm.js:127
+	// Reference: src/admin/settings/src/components/FormRenderer.js:124
 	__( 'Redirect location after login', 'wptelegram-login' ),
 
-	// Reference: src/admin/settings/components/SettingsForm.js:129
+	// Reference: src/admin/settings/src/components/FormRenderer.js:126
 	__( 'Default', 'wptelegram-login' ),
 
-	// Reference: src/admin/settings/components/SettingsForm.js:130
+	// Reference: src/admin/settings/src/components/FormRenderer.js:127
 	__( 'Homepage', 'wptelegram-login' ),
 
-	// Reference: src/admin/settings/components/SettingsForm.js:131
+	// Reference: src/admin/settings/src/components/FormRenderer.js:128
 	__( 'Current page', 'wptelegram-login' ),
 
-	// Reference: src/admin/settings/components/SettingsForm.js:132
-	// Reference: src/admin/settings/fields.js:77
+	// Reference: src/admin/settings/src/components/FormRenderer.js:129
+	// Reference: src/admin/settings/src/fields.js:84
 	__( 'Custom URL', 'wptelegram-login' ),
 
-	// Reference: src/admin/settings/components/SettingsForm.js:154
+	// Reference: src/admin/settings/src/components/FormRenderer.js:153
 	__( 'The user meta key to be used to save Telegram photo URL.', 'wptelegram-login' ),
 
-	// Reference: src/admin/settings/components/SettingsForm.js:167
+	// Reference: src/admin/settings/src/components/FormRenderer.js:168
+	__( 'Generate a random email address', 'wptelegram-login' ),
+
+	// Reference: src/admin/settings/src/components/FormRenderer.js:170
+	__( 'If checked, a random email address will be generated for new user accounts.', 'wptelegram-login' ),
+
+	// Reference: src/admin/settings/src/components/FormRenderer.js:172
+	__( 'Useful when you want the users to be able to receive private notifications.', 'wptelegram-login' ),
+
+	// Reference: src/admin/settings/src/components/FormRenderer.js:182
 	__( 'Button Options', 'wptelegram-login' ),
 
-	// Reference: src/admin/settings/components/SettingsForm.js:172
+	// Reference: src/admin/settings/src/components/FormRenderer.js:187
 	__( 'Large', 'wptelegram-login' ),
 
-	// Reference: src/admin/settings/components/SettingsForm.js:173
+	// Reference: src/admin/settings/src/components/FormRenderer.js:188
 	__( 'Medium', 'wptelegram-login' ),
 
-	// Reference: src/admin/settings/components/SettingsForm.js:174
+	// Reference: src/admin/settings/src/components/FormRenderer.js:189
 	__( 'Small', 'wptelegram-login' ),
 
-	// Reference: src/admin/settings/components/SettingsForm.js:183
+	// Reference: src/admin/settings/src/components/FormRenderer.js:198
 	__( 'Display Telegram user profile photo beside button', 'wptelegram-login' ),
 
-	// Reference: src/admin/settings/components/SettingsForm.js:211
+	// Reference: src/admin/settings/src/components/FormRenderer.js:226
 	__( 'Who can see the login button', 'wptelegram-login' ),
 
-	// Reference: src/admin/settings/components/SettingsForm.js:222
+	// Reference: src/admin/settings/src/components/FormRenderer.js:237
 	__( 'Hide the button on default WordPress login/register page', 'wptelegram-login' ),
 
-	// Reference: src/admin/settings/components/SettingsForm.js:234
+	// Reference: src/admin/settings/src/components/FormRenderer.js:249
 	__( 'Display an error message if Telegram is blocked by user\'s ISP', 'wptelegram-login' ),
 
-	// Reference: src/admin/settings/components/SettingsForm.js:244
+	// Reference: src/admin/settings/src/components/FormRenderer.js:260
 	__( 'Leave empty for default', 'wptelegram-login' ),
 
-	// Reference: src/admin/settings/components/SettingsForm.js:256
-	__( 'Changes saved successfully.', 'wptelegram-login' ),
+	// Reference: src/admin/settings/src/components/FormRenderer.js:272
+	__( 'Save Changes', 'wptelegram-login' ),
 
-	// Reference: src/admin/settings/components/SettingsForm.js:27
+	// Reference: src/admin/settings/src/components/FormRenderer.js:33
 	__( 'Could not connect', 'wptelegram-login' ),
 
-	// Reference: src/admin/settings/components/SettingsForm.js:59
+	// Reference: src/admin/settings/src/components/FormRenderer.js:44
 	__( 'Telegram Options', 'wptelegram-login' ),
 
-	// Reference: src/admin/settings/components/SettingsForm.js:63
+	// Reference: src/admin/settings/src/components/FormRenderer.js:48
 	__( 'Please read the instructions above', 'wptelegram-login' ),
 
-	// Reference: src/admin/settings/components/SettingsForm.js:77
+	// Reference: src/admin/settings/src/components/FormRenderer.js:70
 	__( 'Please wait...', 'wptelegram-login' ),
 
-	// Reference: src/admin/settings/components/SettingsForm.js:86
+	// Reference: src/admin/settings/src/components/FormRenderer.js:79
 	__( 'Test Token', 'wptelegram-login' ),
 
-	// Reference: src/admin/settings/components/SettingsForm.js:86
+	// Reference: src/admin/settings/src/components/FormRenderer.js:79
 	__( 'Use %s above to set automatically.', 'wptelegram-login' ),
 
-	// Reference: src/admin/settings/components/SocialIcons.js:20
+	// Reference: src/admin/settings/src/components/FormRenderer.js:94
+	__( 'Login Options', 'wptelegram-login' ),
+
+	// Reference: src/admin/settings/src/components/FormRenderer.js:98
+	__( 'Disable Sign up', 'wptelegram-login' ),
+
+	// Reference: src/admin/settings/src/components/FormRenderer.js:99
+	__( 'If checked, only the existing users who have connected their Telegram will be able to login.', 'wptelegram-login' ),
+
+	// Reference: src/admin/settings/src/components/Header.js:28
+	__( 'With this plugin, you can let the users login to your website with their Telegram and make it simple for them to get connected.', 'wptelegram-login' ),
+
+	// Reference: src/admin/settings/src/components/Instructions.js:13
+	__( 'INSTRUCTIONS!', 'wptelegram-login' ),
+
+	// Reference: src/admin/settings/src/components/PluginInfoCard.js:16
+	__( 'Let the users login to your WordPress website with their Telegram and make it simple for them to get connected and let them receive their email notifications on Telegram.', 'wptelegram-login' ),
+
+	// Reference: src/admin/settings/src/components/SocialIcons.js:29
 	__( 'Follow %s', 'wptelegram-login' ),
 
-	// Reference: src/admin/settings/components/SocialIcons.js:29
+	// Reference: src/admin/settings/src/components/SocialIcons.js:49
 	__( 'Join %s', 'wptelegram-login' ),
 
-	// Reference: src/admin/settings/components/WidgetInfoCard.js:17
+	// Reference: src/admin/settings/src/components/WidgetInfoCard.js:12
 	__( 'Widget Info', 'wptelegram-login' ),
 
-	// Reference: src/admin/settings/components/WidgetInfoCard.js:20
-	__( 'Goto %1$s and click/drag %2$s and place it where you want it to be.', 'wptelegram-login' ),
-
-	// Reference: src/admin/settings/components/WidgetInfoCard.js:20
-	__( 'Appearance', 'wptelegram-login' ),
-
-	// Reference: src/admin/settings/components/WidgetInfoCard.js:20
-	__( 'Widgets', 'wptelegram-login' ),
-
-	// Reference: src/admin/settings/components/WidgetInfoCard.js:23
-	__( 'Alternately, you can use the below shortCode or the block available in block editor.', 'wptelegram-login' ),
-
-	// Reference: src/admin/settings/components/WidgetInfoCard.js:26
-	__( 'Inside page or post content:', 'wptelegram-login' ),
-
-	// Reference: src/admin/settings/components/WidgetInfoCard.js:32
-	__( 'Inside the theme templates', 'wptelegram-login' ),
-
-	// Reference: src/admin/settings/components/WidgetInfoCard.js:37
-	__( 'or', 'wptelegram-login' ),
-
-	// Reference: src/admin/settings/fields.js:58
+	// Reference: src/admin/settings/src/fields.js:65
 	__( 'Invalid %s', 'wptelegram-login' ),
 
-	// Reference: src/admin/settings/fields.js:61
+	// Reference: src/admin/settings/src/fields.js:68
 	__( '%s is required.', 'wptelegram-login' ),
 
-	// Reference: src/admin/settings/fields.js:65
+	// Reference: src/admin/settings/src/fields.js:72
 	__( 'Changes could not be saved.', 'wptelegram-login' ),
 
-	// Reference: src/admin/settings/fields.js:72
+	// Reference: src/admin/settings/src/fields.js:79
 	__( 'Bot Token', 'wptelegram-login' ),
 
-	// Reference: src/admin/settings/fields.js:73
+	// Reference: src/admin/settings/src/fields.js:80
 	__( 'Bot Username', 'wptelegram-login' ),
 
-	// Reference: src/admin/settings/fields.js:74
+	// Reference: src/admin/settings/src/fields.js:81
 	__( 'Sign up', 'wptelegram-login' ),
 
-	// Reference: src/admin/settings/fields.js:75
+	// Reference: src/admin/settings/src/fields.js:82
 	__( 'User Role', 'wptelegram-login' ),
 
-	// Reference: src/admin/settings/fields.js:76
+	// Reference: src/admin/settings/src/fields.js:83
 	__( 'Redirect to', 'wptelegram-login' ),
 
-	// Reference: src/admin/settings/fields.js:78
+	// Reference: src/admin/settings/src/fields.js:85
 	__( 'Avatar URL Meta Key', 'wptelegram-login' ),
 
-	// Reference: src/admin/settings/fields.js:83
+	// Reference: src/admin/settings/src/fields.js:86
+	__( 'Random Email', 'wptelegram-login' ),
+
+	// Reference: src/admin/settings/src/fields.js:91
 	__( 'Hide on default login', 'wptelegram-login' ),
 
-	// Reference: src/admin/settings/fields.js:84
+	// Reference: src/admin/settings/src/fields.js:92
 	__( 'Show error message', 'wptelegram-login' ),
 
-	// Reference: src/admin/settings/fields.js:85
+	// Reference: src/admin/settings/src/fields.js:93
 	__( 'Error message text', 'wptelegram-login' )
 );
 /* THIS IS THE END OF THE GENERATED FILE */
