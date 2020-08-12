f1ef54375bf88848c82b3ace252e432aa6a5af61
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-08-13 01:56:45 +0530

    Update packages and webpack config

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -2,7 +2,7 @@ import path from 'path';
 // import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import autoprefixer from 'autoprefixer';
 // import LodashModuleReplacementPlugin from 'lodash-webpack-plugin';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin-with-rtl';
+import MiniCssExtractPlugin from '@automattic/mini-css-extract-plugin-with-rtl';
 // import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import WebpackRTLPlugin from 'webpack-rtl-plugin';
 import camelCase from 'camelCase';
@@ -12,11 +12,14 @@ import camelCase from 'camelCase';
  */
 import { createConfig } from './tools/webpack';
 
-const mainSettings = (isDev) => {
-	return createConfig({
-		entry: ['regenerator-runtime/runtime', './src/admin/settings/src/index.js'],
+const mainSettings = ( isDev ) => {
+	return createConfig( {
+		entry: [
+			'regenerator-runtime/runtime',
+			'./src/admin/settings/src/index.js',
+		],
 		output: {
-			path: path.resolve(__dirname, 'src/admin/settings/dist'),
+			path: path.resolve( __dirname, 'src/admin/settings/dist' ),
 			filename: 'settings-dist.js',
 			chunkFilename: '[name].js',
 		},
@@ -26,7 +29,9 @@ const mainSettings = (isDev) => {
 					test: /\.css$/, // .less and .css
 					use: [
 						{
-							loader: isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+							loader: isDev
+								? 'style-loader'
+								: MiniCssExtractPlugin.loader,
 						},
 						{
 							loader: 'css-loader',
@@ -37,36 +42,36 @@ const mainSettings = (isDev) => {
 		},
 		plugins: [
 			// new BundleAnalyzerPlugin(),
-			new MiniCssExtractPlugin({
+			new MiniCssExtractPlugin( {
 				filename: 'settings-dist.css',
 				chunkFilename: '[name].css',
 				rtlEnabled: true,
-			}),
+			} ),
 			new WebpackRTLPlugin(),
 		],
-	});
+	} );
 };
 
-const blocks = (isDev) => {
-	const settings = mainSettings(isDev);
+const blocks = ( isDev ) => {
+	const settings = mainSettings( isDev );
 
 	const externals = {};
 	// Define WordPress dependencies
-	const wpPackages = ['block-editor', 'blocks', 'components', 'i18n'];
+	const wpPackages = [ 'block-editor', 'blocks', 'components', 'i18n' ];
 	// Setup externals for all WordPress dependencies
-	wpPackages.forEach((wpPackage) => {
-		externals['@wordpress/' + wpPackage] = {
+	wpPackages.forEach( ( wpPackage ) => {
+		externals[ '@wordpress/' + wpPackage ] = {
 			this: [
 				'wp',
-				wpPackage.includes('-') ? camelCase(wpPackage) : wpPackage, // 'block-editor' => 'blockEditor'
+				wpPackage.includes( '-' ) ? camelCase( wpPackage ) : wpPackage, // 'block-editor' => 'blockEditor'
 			],
 		};
-	});
+	} );
 	return {
 		...settings,
 		entry: './src/admin/blocks/src/index.js',
 		output: {
-			path: path.resolve(__dirname, 'src/admin/blocks/dist'),
+			path: path.resolve( __dirname, 'src/admin/blocks/dist' ),
 			filename: 'blocks-build.js',
 			libraryTarget: 'this',
 		},
@@ -76,10 +81,11 @@ const blocks = (isDev) => {
 		},
 		plugins: [
 			// new BundleAnalyzerPlugin(),
-			new MiniCssExtractPlugin({
+			new MiniCssExtractPlugin( {
 				filename: 'blocks-build.css',
 				rtlEnabled: true,
-			}),
+				rtlGlobalVar: 'isRtl',
+			} ),
 			new WebpackRTLPlugin(),
 		],
 		module: {
@@ -96,16 +102,17 @@ const blocks = (isDev) => {
 							options: {
 								ident: 'postcss',
 								plugins: [
-									autoprefixer({
+									autoprefixer( {
 										flexbox: 'no-2009',
-									}),
+									} ),
 								],
 							},
 						},
 						{
 							loader: 'sass-loader',
 							options: {
-								prependData: '@import "./src/admin/blocks/src/common.scss";\n',
+								// additionalData:
+								// 	'@import "./src/admin/blocks/src/common.scss";\n',
 								sassOptions: {
 									outputStyle: 'expanded',
 								},
@@ -119,9 +126,9 @@ const blocks = (isDev) => {
 	};
 };
 
-const configs = (env, argv = {}) => {
+const configs = ( env, argv = {} ) => {
 	const isDev = 'development' === argv.mode;
-	return [mainSettings(isDev), blocks(isDev)];
+	return [ mainSettings( isDev ), blocks( isDev ) ];
 };
 
 export default configs;
