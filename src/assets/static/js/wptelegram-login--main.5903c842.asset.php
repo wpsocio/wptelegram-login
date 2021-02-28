4da601e5fe9122f051616b4aea1e3ae11b9c25fa
Manzoor Wani <manzoorwani.jk@gmail.com>
2021-02-28 19:07:58 +0530

    Fix deploy workflow

--- src/assets/static/js/wptelegram-login--main.5903c842.asset.php
+++ src/assets/static/js/wptelegram-login--main.5903c842.asset.php
@@ -1,5 +1,1 @@
-<<<<<<< HEAD:src/assets/static/js/wptelegram-login--main.ff8f9be3.asset.php
-<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-api-fetch', 'wp-i18n', 'wp-polyfill'), 'version' => '0e1b75e50c857b5a1a8a252e2d394f0e');
-=======
-<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-api-fetch', 'wp-i18n', 'wp-polyfill'), 'version' => '3b06d0c6e0a4e96bd4c9ce94d624b204');
->>>>>>> master:src/assets/static/js/wptelegram-login--main.5903c842.asset.php
+<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-api-fetch', 'wp-i18n', 'wp-polyfill'), 'version' => '3b06d0c6e0a4e96bd4c9ces94d624b204');
