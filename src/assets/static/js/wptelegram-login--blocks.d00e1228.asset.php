5b2bc45745001ede53a64611e21028d146b6c632
GitHub Action <github-action@users.noreply.github.com>
2021-03-19 21:29:58 +0000

    Deployed from manzoorwanijk/wp-plugins@eab2d7fbd364c24e7323e5d9ef71b9cb8d49aa4c

---
+++ src/assets/static/js/wptelegram-login--blocks.d00e1228.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '068884a68d01d47b2e9a04978d5b6078');
--- src/assets/static/js/wptelegram-login--blocks.f7aa31f9.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'b3bfc2438a60c0e0fe900e3105c0c0f9');
