fbca71faec0569c2cefff0c639a8aa8d4bf21a71
Irshad Ahmad <irshad@wpsocio.com>
2024-10-01 13:08:50 +0000

    Deployed from wpsocio/wp-projects@a5dcc72f12ab5e7ecedf6d67fb8d4b102c03befb

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,8 +1,8 @@
 <?php return array(
     'root' => array(
         'name' => 'wpsocio/wptelegram-login',
-        'pretty_version' => '1.10.22',
-        'version' => '1.10.22.0',
+        'pretty_version' => '1.10.23',
+        'version' => '1.10.23.0',
         'reference' => null,
         'type' => 'library',
         'install_path' => __DIR__ . '/../../../',
@@ -20,8 +20,8 @@
             'dev_requirement' => false,
         ),
         'wpsocio/wptelegram-login' => array(
-            'pretty_version' => '1.10.22',
-            'version' => '1.10.22.0',
+            'pretty_version' => '1.10.23',
+            'version' => '1.10.23.0',
             'reference' => null,
             'type' => 'library',
             'install_path' => __DIR__ . '/../../../',
